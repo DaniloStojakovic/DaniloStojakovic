e1b4558f8db5c4111476e07a015639eb1cd07de4
DaniloStojakovic <fqwdqdwqd@gmail.com>
2022-06-10 19:03:29 +0200

    Add project files.

---
+++ Client/Client.py
@@ -0,0 +1,23 @@
+
+import socket
+from time import sleep
+
+ClientSocket = socket.socket()
+host = 'localhost'
+port = 5555
+
+print('Waiting for connection')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+'''Response = ClientSocket.recv(1024)
+while True:
+    #Input = input('Say Something: ')
+    ClientSocket.send(str.encode("Test"))
+    Response = ClientSocket.recv(1024)
+    sleep(5)
+    print(Response.decode('utf-8'))
+
+ClientSocket.close()'''
